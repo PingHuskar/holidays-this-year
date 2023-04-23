8f15ab1119ce079b6e454f9759a019aec1622a73
Chadin Chaipornpisuth <ping47024@icloud.com>
2023-04-23 22:10:00 +0700

    add thaiholidays dict

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,34 @@ const months: any = {
   "en": ["January","February","March","April","May","June","July","August","September","October","November","December"],
   "th": [ "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"],
 }
-const dicts: any = {
-  daysago: {
-    "en": `days ago`,
-    "th": `วันที่แล้ว`,
-  },
-  nextxdays: {
-    "en": `coming in`,
-    "th": `รออีก`,
-  },
+const dicts = (name: string, lang: string, num: number) => {
+  switch (name) {
+    case `daysago`:
+      return lang === "en" ? `${num-1} days ago` : `${num-1} วันที่ผ่านมา`
+    case `nextxdays`:
+      return lang === "en" ? `coming in ${Math.abs(num)+1} days` : `จะถึงในอีก ${Math.abs(num)+1} วัน`
+    default:
+      return ``
+  }
+}
+
+const ThaiHolidaysDict: any = {
+  "New Year's Day": `วันปีใหม่`,
+  "Makha Bucha": `วันมาฆบูชา`,
+  "Chakri Memorial Day": `วันจักรี`,
+  "Songkran Festival": `วันสงกรานต์`,
+  "Coronation Day": `วันฉัตรมงคล`,
+  "Vesak Day": `วันวิสาขบูชา`,
+  "Queen Suthida's Birthday": `วันเฉลิมพระชนมพรรษาสมเด็จพระราชินิ`,
+  "King's Birthday": `วันเฉลิมพระชนมพรรษา`,
+  "Asalha Puja": `วันอาสาฬหบูชา`,
+  "Buddhist Lent": `วันเข้าพรรษา`,
+  "The Queen Mother's Birthday": `วันเฉลิมพระชนมพรรษา สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ พระบรมราชชนนีพันปีหลวง`,
+  "King Bhumibol Adulyadej Memorial Day": `วันคล้ายวันสวรรคต พระบาทสมเด็จพระปรมินทรมหาภูมิพลอดุลยเดช บรมนาถรบพิตร`,
+  "King Chulalongkorn Day": `วันปิยมหาราช`,
+  "King Bhumibol Adulyadej's Birthday": `วันคล้ายวันพระราชสมภพของพระบาทสมเด็จพระบรม ชนกาธิเบศร มหาภูมิพลอดุลยเดชมหาราช บรมนาถบพิตร`,
+  "Constitution Day": `วันรัฐธรรมนูญ`,
+  "New Year's Eve": `วันสิ้นปี`,
 }
 
 function App() {
@@ -38,13 +57,22 @@ function App() {
           setLang(ll)
         }}>{l.toLocaleUpperCase()}</span>
       })}</h2>
+      <hr />
       <ol>
         {hd.getHolidays().map((d,i)=> {
           const daysago = parseInt(moment.duration(TODAYDATE.diff(d.start)).asDays().toFixed(0))
           return <li key={i}>
-            {/* {d.start.toDateString()} */}
-            {d.start.getDate()} {months[lang][d.start.getMonth()]} &nbsp;
-            ({daysago > 0 ? `${daysago-1} ${dicts.daysago[lang]}` : `${dicts.nextxdays[lang]} ${Math.abs(daysago)+1}`})
+            <span>
+              {lang === `th` ? ThaiHolidaysDict[d.name] : d.name}
+            </span>
+            <span>
+              <span>
+              {d.start.getDate()} {months[lang][d.start.getMonth()]}
+              </span>
+              <span>
+              ({daysago > 0 ? `${dicts(`daysago`,lang,daysago)}` : `${dicts(`nextxdays`,lang,daysago)}`})
+              </span>
+            </span>
           </li>
         })}
       </ol>
